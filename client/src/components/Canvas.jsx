77bf4fe024786759659b6665431443614efeacae
mmwsp <m.manekin99@gmail.com>
2022-12-18 18:20:17 +0100

    modalv2

--- client/src/components/Canvas.jsx
+++ client/src/components/Canvas.jsx
@@ -162,13 +162,13 @@ const Canvas = observer(() => {
         <div className="canvas">
             
             <Modal show={modal} onHide={() => {}}>
-                <Modal.Header >
+                <Modal.Header style={{justifyContent: 'center', backgroundColor: '#E0FFFF'}}>
                     <Modal.Title>Welcome</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
                     <Form>
                         <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                            <Form.Label>Enter your username:</Form.Label>
+                            <Form.Label>Input your username:</Form.Label>
                                 <Form.Control
                                     type="text"
                                     autoFocus
@@ -176,17 +176,18 @@ const Canvas = observer(() => {
                                 />
                         </Form.Group>
                     </Form>
-                    With empty username you can't set connection with other session participants.
+                    <p style={{fontSize: '13px'}}>With empty username you can't set connection with other session participants.</p>
+                    <br></br>
                     <p><b>Share the link above with people you want to draw together.</b></p>
                 </Modal.Body>
-                <Modal.Footer style={{justifyContent: 'center'}}>
+                <Modal.Footer style={{justifyContent: 'center', backgroundColor: '#E0FFFF'}}>
                         <Button variant="outline-secondary" style={{width: '40%'}} size="lg" onClick={() => connectHandler()}>
                             Enter
                         </Button>
                 </Modal.Footer>
             </Modal>
 
-            <canvas onMouseUp={() => mouseUpHandler()} ref={canvasRef} width={1200} height={525}/>
+            <canvas onMouseUp={() => mouseUpHandler()} ref={canvasRef} width={1000} height={650}/>
             <Notification active={notificationActive} setActive={setNotificationActive}>
                 {connection ? `User ${user} has successfully connected` 
                 : `User ${user} has disconnected`}
